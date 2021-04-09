52f3c2c795e69e292a836c0dc3c889b9c312f18e
Nazar Vynnytskyi <nazar.vynnytski@lohika.com>
2021-04-09 13:26:20 +0300

    Improve GalleryPage: useRelayQuery with memo

--- src/pages/GalleryPage/GalleryPage.tsx
+++ src/pages/GalleryPage/GalleryPage.tsx
@@ -1,39 +1,42 @@
-import React, { Suspense, useEffect } from 'react';
-import { useQueryLoader, usePreloadedQuery } from 'react-relay/hooks';
+import React, { Suspense, useEffect, useMemo, useState } from 'react';
+import { usePreloadedQuery } from 'react-relay/hooks';
 
 import MainLayout from '../../layouts/MainLayout/MainLayout';
 import Loader from '../../components/Loader/Loader';
 import GalleryList from './GalleryList/GalleryList';
 import { GalleryPageQuery } from '../../relay/queries';
-// import { useRelayQuery } from '../../relay/hooks';
+import { useRelayQuery } from '../../relay/hooks';
 
 export default function GalleryPage() {
-    const [queryReference, loadQuery, disposeQuery] = useQueryLoader(GalleryPageQuery);
-    useEffect(() => {
-        loadQuery({
-            options: {
-                paginate: {
-                    page: 1,
-                    limit: 24,
-                },
-            },
-        });
-        return () => {
-            disposeQuery();
-        };
-    }, [loadQuery, disposeQuery]);
-
-    // const queryReference = useRelayQuery(GalleryPageQuery, { paginate: { page: 1, limit: 24 } });
+    const [page, setPage] = useState(1);
+    // TODO: Move "useMemo" to "useRelayQuery"
+    const options = useMemo(() => ({ paginate: { page, limit: 24 } }), [page]);
+    const queryReference = useRelayQuery(GalleryPageQuery, options);
 
     return (
         <MainLayout>
             <h1 className="my-4">Gallery</h1>
             <Suspense fallback={<Loader />}>{queryReference && <Container queryReference={queryReference} />}</Suspense>
+            <div className="row">
+                <button className="btn btn-primary mx-auto mb-5" onClick={() => setPage(page + 1)}>
+                    Load more
+                </button>
+            </div>
         </MainLayout>
     );
 }
 
 const Container = ({ queryReference }: { queryReference: any }) => {
-    const { photos } = usePreloadedQuery(GalleryPageQuery, queryReference) as any;
-    return <>{photos?.data?.length > 0 && <GalleryList photos={photos?.data} />}</>;
+    const [photos, setPhotos] = useState([]);
+    const {
+        photos: { data },
+    } = usePreloadedQuery(GalleryPageQuery, queryReference) as any;
+
+    useEffect(() => {
+        if (data?.length) {
+            setPhotos((oldPhotos) => [...oldPhotos.concat(data)]);
+        }
+    }, [data]);
+
+    return <>{photos.length > 0 && <GalleryList photos={photos} />}</>;
 };
