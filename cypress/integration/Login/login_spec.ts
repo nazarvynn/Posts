cfd27d739424e88003bb345d200ddd3c36bb0077
Nazar Vynnytskyi <nazar.vynnytski@lohika.com>
2021-04-26 14:30:39 +0300

    Added cypress folders

--- cypress/integration/Login/login_spec.ts
+++ cypress/integration/Login/login_spec.ts
@@ -1,13 +1,15 @@
+export {};
+
 describe('The Home Page', () => {
     it('successfully loads', () => {
-        // eslint-disable-next-line no-undef
         cy.visit('/');
 
-        // eslint-disable-next-line no-undef
+        cy.contains('Login');
+
         cy.get('input[name="name"]').type('admin');
-        // eslint-disable-next-line no-undef
         cy.get('input[name="password"]').type('password1');
-        // eslint-disable-next-line no-undef
         cy.get('button[type="submit"]').click();
+
+        cy.contains('Posts');
     });
 });
